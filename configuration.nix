42e9abad20ececa715aef9bca73b5cf53986321a
Matt Westrik <m@ttwestrik.com>
2020-10-15 17:19:29 -0700

    copy current config

--- configuration.nix
+++ configuration.nix
@@ -10,220 +10,288 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  networking.hostName = "spinch";
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  boot = {
+      # Use the systemd-boot EFI boot loader.
+      loader.systemd-boot.enable = true;
+      loader.efi.canTouchEfiVariables = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+      # Enable DHCP so networking will work in Stage 1.
+      # This is needed so enough entropy can be collected to recompute the YubiKey challenge-response salt.
+      kernelParams = [
+        "ip=:::::enp4s0:dhcp"
+      ];
 
+      # Load the ZFS kernel modules.
+      supportedFilesystems = [ "zfs" ];
+
+      initrd = {
+        # Install kernel modules needed for networking and YubiKey challenge-response.
+        availableKernelModules = ["igb"];
+        kernelModules = ["vfat" "nls_cp437" "nls_iso8859-1" "usbhid" "igb"];
+        network.enable = true;
+
+        luks = {
+          yubikeySupport = true;
+
+          devices = {
+            cryptroot = {
+              device = "/dev/nvme0n1p2";
+              preLVM = false;
+              yubikey = {
+                slot = 2;
+                twoFactor = false;
+                keyLength = 64; # Set to $KEY_LENGTH/8
+                saltLength = 64; # Set to $SALT_LENGTH
+                storage = {
+                  device = "/dev/nvme0n1p1";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+
+
+  # ------- Locale -------
   # Select internationalisation properties.
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
   };
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
+
+  # ------- Networking -------
+  networking.hostName = "kepchup";
+  networking.hostId = "546f7696";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp4s0.useDHCP = true;
+
+  networking.firewall.enable = true;
+  networking.firewall.allowPing = true;
+  networking.firewall.allowedTCPPorts = [
+    445   # SMB
+    139   # SMB
+    22    # SSH
+    80    # HTTP
+    443   # HTTPS
+    32469 # Plex DLNA
+    3000  # Grafana
+    8000 # Test web service
+    2222 # debian container SSH
+  ];
+  networking.firewall.allowedUDPPorts = [
+    137   # NetBIOS
+    138   # NetBIOS
+    5353  # mDNS (Bonjour)
+  ];
+
+
+  # ------- Dependencies -------
+  # required for nvidia driver, plex, roon, etc.
   nixpkgs.config.allowUnfree = true;
+  # required for steam w/ native libraries
+  nixpkgs.config.allowBroken = true;
+  nixpkgs.overlays = [
+    # required to upgrade roon to >=1.7
+    ( import ./overlays/roon-server.nix )
+  ];
+  environment.systemPackages = with pkgs; [
+    (steam.override { nativeOnly = true; })
 
-  environment = {
-    systemPackages = let pkgsUnstable = import
-    (
-      fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz
-      #fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz
-    )
-    {
-	config.allowUnfree = true;
-    };
-    in
-    with pkgs; [
-	adobeReader
-	clang
-	clipit
-	cmake
-	consul
-	curl
-	ffmpeg
-	firefox
-	gcc
-	gdb
-	gitFull
-	gnumake
-	gnupg
-	inconsolata
-	jdk
-	jq
-	kdeApplications.spectacle
-	kitty
-	neovim
-	nodejs
-	packer
-	#pkgsUnstable.steam
-	pulseaudioFull
-	python3
-	python37Packages.pip
-	qemu
-	ripgrep
-	rustup
-	spotify
-	stack
-	sysbench
-	tdesktop
-	terraform
-	tmux
-	unzip
-	vim
-	wget
-	yarn
-	yubikey-personalization
+    # backups
+    borgbackup
 
-	# QEMU
-	dmg2img
-	kvm
-	libvirt
-	virtmanager
-	qemu
+    # utilities
+    curl
+    fd
+    htop
+    jq
+    ripgrep
+    unzip
+    wget
 
-#      bind
-#      chromium
-#      discord
-#      ffmpeg
-#      file
-#      freerdp
-#      git
-#      git-review
-#      gnome3.gnome-calculator
-#      gnumake
-#      gnutls
-#      gparted
-#      gss
-#      hexchat
-#      irssi
-#      jre
-#      krb5Full
-#      ksuperkey
-#      libreoffice
-#      libu2f-host
-#      ncurses
-#      netcat-openbsd
-#      ntfs3g
-#      ntp
-#      openssl
-#      patchelf
-#      pavucontrol
-#      pciutils
-#      pcsctools
-#      plasma-nm
-#      plasma-workspace-wallpapers
-#      psmisc
-#      python27
-#      python27Packages.pip
-#      python27Packages.virtualenv
-#      python3
-#      python36Packages.flake8
-#      python36Packages.lxml
-#      python36Packages.pymongo
-#      rednotebook
-#      rpm
-#      tmux
-#      trojita
-#      unzip
-#      usbutils
-#      which
-#      xclip
-#      xmlstarlet
-#      xscreensaver
-#      zlib
-    ];
+    # security
+    gnupg
+    yubikey-personalization
+
+    # media
+    imagemagick
+    youtube-dl
+
+    # networking
+    tailscale
+
+    # development tools
+    gitFull
+    neovim
+    tmux
+    tokei
+
+    # development toolchains
+    binutils # ar, etc.
+    bubblewrap
+    clang
+    cmake
+    gnumake
+    libcap
+    nodejs
+    python3
+    python37Packages.pip
+    rustup
+    yarn
+
+    # podman stack
+    # TODO(20.09): replace with virtualisation.podman.enable (see https://nixos.wiki/wiki/Podman)
+    podman
+    runc
+    conmon
+    slirp4netns
+    fuse-overlayfs
+
+    # datastores
+    postgresql
+    mysql
+    libmysqlclient
+    sqlite
+
+  ];
+
+  # Configure shell to use forwarded GPG agent
+  # environment.shellInit = ''
+  #   export GPG_TTY="$(tty)"
+  #   gpg-connect-agent /bye
+  #   export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+  # '';
+  programs.ssh = {
+    startAgent = false;
+    extraConfig = ''
+      StreamLocalBindUnlink yes
+    '';
+  };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    enableExtraSocket = true;
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # List services that you want to enable:
-
-  programs.zsh.enable = true;
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  sound.enable = true;
-
-  hardware = {
-    opengl.driSupport32Bit = true;
-    pulseaudio.enable = true;
-    pulseaudio.support32Bit = true;
-    bluetooth.enable = false;
-  };
-
-
+  # ------- Services -------
   services.xserver = {
     enable = true;
     layout = "us";
-    desktopManager.plasma5.enable = true;
-    desktopManager.default = "plasma5";
-    videoDrivers = [ "mesa" ];
+    videoDrivers = [ "nvidia" ];
+    desktopManager.xfce.enable = true;
   };
 
-#  services.xserver = {
-#    enable = true;
-#    layout = "us";
-#
-#    desktopManager = {
-#      default = "none";
-#      xterm.enable = false;
-#    };
-#
-#    windowManager.i3 = {
-#      enable = true;
-#      extraPackages = with pkgs; [
-#        dmenu #application launcher most people use
-#        i3status # gives you the default i3 status bar
-#        i3lock #default i3 screen locker
-#        i3blocks #if you are planning on using i3blocks over i3status
-#     ];
-#    };
-#  };
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
-  # Enable yubikey
-  services.pcscd.enable = true;
-  services.udev.packages = [
-    pkgs.libu2f-host
-    pkgs.yubikey-personalization
-  ];
+  # needed for steam
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.pulseaudio.support32Bit = true;
+
+  services.openssh = {
+    enable = true;
+    forwardX11 = true;
+    permitRootLogin = "no";
+  };
+
+  services.tailscale.enable = true;
 
   services.samba = {
     enable = true;
-    nsswins = true;
+    enableWinbindd = false;
+    shares.media = {
+       path = "/media";
+       "valid users" = "matt";
+       "force user" = "matt";
+       public = "no";
+       writable = "yes";
+       "fruit:aapl" = "yes";
+       # store macOS metadata
+       "fruit:metadata" = "stream";
+    };
+    shares.backups = {
+       path = "/backups";
+       "valid users" = "matt betty";
+       public = "no";
+       writeable = "yes";
+       "fruit:aapl" = "yes";
+       # store macOS metadata
+       "fruit:metadata" = "stream";
+       # allow time machine backup
+       "fruit:time machine" = "yes";
+       "vfs objects" = "catia fruit streams_xattr";
+    };
+    extraConfig = ''
+      # Disable printing
+      load printers = no
+      printing = bsd
+      printcap name = /dev/null
+      disable spoolss = yes
+    '';
   };
 
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.roon-server = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.apcupsd.enable = true;
+
+  services.prometheus.exporters.node = {
+    enable = true;
+    enabledCollectors = [
+      "logind"
+      "systemd"
+    ];
+    disabledCollectors = [
+      "textfile"
+    ];
+    openFirewall = true;
+  };
+
+  virtualisation.docker.enable = true;
+
+  # ------- Users & Groups -------
   users.users.matt = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    shell = pkgs.zsh;
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
+    # TODO(20.09): remove these (for podman)
+    subUidRanges = [{ startUid = 100000; count = 65536; }];
+    subGidRanges = [{ startGid = 100000; count = 65536; }];
   };
+  users.users.betty = { isNormalUser = true; };
 
-  # This value determines the NixOS release with which your system is to be
-  # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "19.03"; # Did you read the comment?
+  # grant access to media files
+  users.users.roon-server = { extraGroups = [ "users" ]; };
+  users.users.plex = { extraGroups = [ "users" ]; };
+
+
+  # ------- Release -------
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+
 }
